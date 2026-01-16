6ef41ad475baf16f217e7335854d779626e0acb3
SachkoAP <131907795+SachkoAP@users.noreply.github.com>
2026-01-16 22:59:20 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,11 +1,9 @@
-const themes=["Мысли","Философия","Путешествия","Идеи"];
-let articles=JSON.parse(localStorage.getItem('articles'))||[];
-
-// =========================
-// js/telegram.js
-// =========================
-const tg=window.Telegram.WebApp;tg.ready();
-const OWNER_USERNAME="ТВОЙ_USERNAME";
-if(tg.initDataUnsafe?.user?.username===OWNER_USERNAME){
-document.getElementById('addBtn')?.classList.remove('hidden');
+const themes = ["Мысли","Философия","Жизнь","Наблюдения","Идеи"];
+let articles = JSON.parse(localStorage.getItem("articles")) || [
+{
+title:"Почему мысли путаются",
+text:"Иногда кажется, что мозг — это тарелка спагетти...",
+date:"Сегодня",
+image:null
 }
+];
